2047cf03f5b023895118eb47ff70b55e0256dcfd
wajid hussain kashoo <i.wajid.hussain@generalaeronautics.com>
2024-01-20 16:35:06 +0530

    code fix

--- src/app/hotels/[location]/page.js
+++ src/app/hotels/[location]/page.js
@@ -13,22 +13,22 @@ function page() {
   const destination = data.filter((hotel) => hotel.location === stay)[0];
 
   return (
-    <div class="min-h-screen flex flex-col justify-center items-center gap-10 mb-24">
+    <div class="min-h-screen flex flex-wrap justify-center items-center gap-10 mb-24">
       <h1 class="text-3xl my-12 font-semibold">{`Hotels in ${
         stay.charAt(0).toUpperCase() + stay.slice(1)
       }`}</h1>
       {destination?.hotels && destination.hotels.length ? destination?.hotels.map((hotel) => (
-        <div class="w-3/4 flex justify-items-start items-start gap-14 text-left border-solid border-2 border-[#F4F4F4] relative cursor-pointer shadow-xl">
+        <div class="w-1/3 flex justify-items-start items-start gap-6 text-left border-solid border-2 border-[#F4F4F4] relative cursor-pointer shadow-xl">
           <Image
             src={hotel?.image}
             sizes="100vw"
-            width={500}
-            height={300}
+            width={300}
+            height={200}
             alt="dallake Image"
             loading="lazy"
             style={{
-              width: "600px",
-              height: "300px",
+              width: "300px",
+              height: "200px",
             }}
           />
           <div class="p-4 flex flex-col gap-10">
@@ -36,7 +36,7 @@ function page() {
             <p class="text-left text-thin leading-7">
               {hotel.description}
             </p>
-            <span class="flex items-center gap-5"><FaLocationDot />{hotel?.address}</span>
+            <span class="flex items-center gap-5"><FaLocationDot />{hotel?.location}</span>
             <span className={styles.star}>
               {Array.from({ length: hotel.stars }, (_, i) => (
                 <FaStar key={i} />
