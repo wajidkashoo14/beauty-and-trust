2c36289d056d5d87e5149b243d5dc96c3baead6f
wajid hussain kashoo <i.wajid.hussain@generalaeronautics.com>
2024-01-22 13:04:56 +0530

    navbar responsive styles

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,14 +1,19 @@
+"use client"
 import Image from "next/image";
 import Link from "next/link";
 import { FaWhatsapp } from "react-icons/fa";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoMdClose } from "react-icons/io";
+import { useState } from "react";
 
 function Navbar() {
+  const [nav, setNav] = useState(false);
   return (
     <nav class=" border-gray-200 bg-[#024F74] w-full poppins fixed top-0 z-50">
-      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-4 py-5">
+      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-4 py-5 max-[600px]:hidden">
         <Link
           href="/"
-          class="text-white font-semibold text-xl flex justify-center items-center gap-2 -z-1"
+          class="text-white font-semibold text-xl flex justify-center items-center gap-2 -z-1 [600px]:hidden"
         >
           <Image
             src="/logo.jpg"
@@ -28,7 +33,7 @@ function Navbar() {
           >
             <FaWhatsapp color="#fff" fontSize={20} /> WhatsApp
           </Link>
-          <button
+          {/* <button
             data-collapse-toggle="navbar-cta"
             type="button"
             class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
@@ -51,7 +56,7 @@ function Navbar() {
                 d="M1 1h15M1 7h15M1 13h15"
               />
             </svg>
-          </button>
+          </button> */}
         </div>
         <div
           class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1 poppins z-50"
@@ -108,6 +113,97 @@ function Navbar() {
               </Link>
             </li>
           </ul>
+        </div>
+      </div>
+      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-4 py-3 max-[600px]:flex max-[600px]:justify-between">
+        <Link
+          href="/"
+          class="text-white font-semibold text-xl flex justify-center items-center gap-2 -z-1"
+        >
+          <Image
+            src="/logo.jpg"
+            alt="logo"
+            loading="lazy"
+            width={60}
+            height={60}
+            class="rounded-full"
+          />
+        </Link>
+        <span className="text-normal text-lg text-green-300 w-36">Beauty and Trust Tour and Travels</span>
+        <div class="flex">
+          <div onClick={() => setNav(!nav)}>
+            {nav ? (
+              <IoMdClose size={30} color="#B8BDC6" />
+            ) : (
+              <RxHamburgerMenu size={30} color="#B8BDC6" />
+            )}
+          </div>
+
+          
+          {/* <Link
+            href="https://wa.me/917051695835"
+            type="button"
+            class="text-white bg-[#25D366]  font-medium text-sm px-6 py-3 text-center flex items-center justify-center gap-1 max-[600px]:absolute max-[600px]:bottom-5 max-[600px]:right-4"
+          >
+            <FaWhatsapp color="#fff" fontSize={20} />
+          </Link> */}
+         {nav ? <ul class="flex flex-col font-medium text-center pt-20 bg-[#024F74] h-screen w-full mt-10 absolute top-10 left-0">
+          <li>
+            <Link
+              href="/"
+              class="block py-2 px-3 md:p-0 text-white bg-transparent"
+              aria-current="page"
+              onClick={() => setNav(false)}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/#tours"
+              class="block py-2 px-3 md:p-0 text-white bg-transparent"
+              onClick={() => setNav(false)}
+            >
+              Tour
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/#hotels"
+              class="block py-2 px-3 md:p-0 text-white bg-transparent"
+              onClick={() => setNav(false)}
+            >
+              Hotels
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/transport"
+              class="block py-2 px-3 md:p-0 text-white bg-transparent"
+              onClick={() => setNav(false)}
+            >
+              Transport
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/aboutus"
+              class="block py-2 px-3 md:p-0 text-white rounded bg-transparent"
+              onClick={() => setNav(false)}
+            >
+              About us
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/contact"
+              class="block py-2 px-3 md:p-0 text-white rounded bg-transparent"
+              onClick={() => setNav(false)}
+            >
+              Contact
+            </Link>
+          </li>
+        </ul> : null}
         </div>
       </div>
     </nav>
